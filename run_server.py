c1d0e1ddf3c16d978f68fb22fe2fe54fd0493b64
icleoc <153005082+icleoc@users.noreply.github.com>
2025-11-09 00:16:02 -0300

    Update run_server.py

--- run_server.py
+++ run_server.py
@@ -1,10 +1,7 @@
-# run_server.py
 from monitor_vwap_real import app, start_bot
 from threading import Thread
 
-# Inicia o bot em background
 Thread(target=start_bot, daemon=True).start()
 
-# Executa o Flask
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
