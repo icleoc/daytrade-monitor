00e2df44470283bc431b4ca8d34b652b24ec2a4a
icleoc <153005082+icleoc@users.noreply.github.com>
2025-11-10 09:22:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-# app.py
 from flask import Flask, jsonify, render_template
-from monitor.monitor_vwap_real import get_all_vwap_data
+from monitor.monitor_vwap_real import get_all_assets_data
 
-app = Flask(__name__, static_folder="static", template_folder="templates")
+app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("dashboard.html")
 
-@app.route("/api/vwap")
-def api_vwap():
-    data = get_all_vwap_data()
+@app.route("/vwap")
+def vwap_data():
+    data = get_all_assets_data()
     return jsonify(data)
 
 if __name__ == "__main__":
