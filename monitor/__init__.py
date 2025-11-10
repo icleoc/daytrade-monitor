42e5b891e1063cfc6e6d05a10763922e1d976072
icleoc <153005082+icleoc@users.noreply.github.com>
2025-11-09 23:12:47 -0300

    Update __init__.py

--- monitor/__init__.py
+++ monitor/__init__.py
@@ -1,3 +1,2 @@
-from .monitor_vwap import run_monitor
-from .monitor_indicators import moving_average
-from .alerts import check_threshold
+# monitor/__init__.py
+# Inicialização simples para o módulo monitor
