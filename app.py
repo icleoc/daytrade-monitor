eaefa23d87f0dcaca8d7381d0948beac2ca5c52c
icleoc <153005082+icleoc@users.noreply.github.com>
2025-11-10 09:15:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # app.py
 from flask import Flask, jsonify, render_template
 from monitor.monitor_vwap_real import get_all_vwap_data
-import os
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
 
@@ -12,8 +11,7 @@ def index():
 @app.route("/api/vwap")
 def api_vwap():
     data = get_all_vwap_data()
-    return jsonify({"assets": data})
+    return jsonify(data)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
