496f8d184c5eb4cd279e407311a9018608e54c50
icleoc <153005082+icleoc@users.noreply.github.com>
2025-11-12 18:04:18 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 SYMBOLS = [
-    {"symbol": "BTCUSD"},
-    {"symbol": "ETHUSD"},
-    {"symbol": "EURUSD"},
-    {"symbol": "XAUUSD"},
+    {"symbol": "BTCUSD", "name": "Bitcoin"},
+    {"symbol": "ETHUSD", "name": "Ethereum"},
+    {"symbol": "EURUSD", "name": "Euro / Dólar"},
+    {"symbol": "XAUUSD", "name": "Ouro"},
 ]
 
 # Configurações gerais
 CANDLE_LIMIT = 120
 TIMEFRAME_MINUTES = 15
+
+
+
