3a8fec4673e09f64deeba105b7bdf5d37196620f
icleoc <153005082+icleoc@users.noreply.github.com>
2025-11-12 18:31:12 -0300

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ SYMBOLS = [
     {"symbol": "EURUSD", "name": "Euro / Dólar"},
     {"symbol": "XAUUSD", "name": "Ouro"},
 ]
+# config.py
+# Intervalo de atualização da API (em segundos)
+UPDATE_INTERVAL_SECONDS = 15
 
 
 
